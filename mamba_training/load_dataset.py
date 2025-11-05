fab986e6c2d87782456b2702311b23fb39894c47
Anjana Anand <116085412+anjana5anand@users.noreply.github.com>
2025-11-05 13:10:00 -0700

    Delete mamba_training directory

--- mamba_training/load_dataset.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("parquet", data_files="/media/viplab/DATADRIVE1/huggingface/ucf101/test/*.parquet")
-
-
-print(dataset)
--- mamba_training/ufc101_download.py
+++
@@ -1,6 +0,0 @@
-import os
-from datasets import load_dataset
-
-ds = load_dataset("flwrlabs/ucf101")
-
-print(ds)
